2a7260cf1bc3f86e8123c478036dc185ea64c49e
niklusrook <nikolas.vardeberg1@gmail.com>
2025-04-29 17:17:21 +0200

    feat: render products in productGrid

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -119,6 +119,35 @@ const ProductGrid = ({ products }: Props) => {
                     </Sheet>
                 </div>
             </header>
+
+            <main className="container mx-auto px-4 py-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-40">
+                    {products.map((product, index) => (
+                        <Link
+                            href={`/products/${product.id}`}
+                            key={index}
+                            className="flex flex-col group relative"
+                        >
+                            <div className="aspect-square rounded-md overflow-hidden">
+                                <Image
+                                    src={product.images[0].src || "/placeholder.png"}
+                                    alt={product.name}
+                                    width={300}
+                                    height={300}
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
+                            <div className="group-hover:opacity-100 opacity-0 flex flex-col justify-end">
+                                <div className="flex justify-between items-center mb-2">
+                                    <h3 className="text-base font-semibold text-primary">{product.name}</h3>
+
+                                    <p className="text-base font-semibold text-primary">${product.price}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </main>
         </div>
     )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { Turret_Road } from "next/font/google";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  }
 };
 
 export default nextConfig;
